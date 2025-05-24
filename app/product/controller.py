e5c0ba37a38cccb31423ac380604549ff16611fb
GuilhermeLopesBertacini <guilb2005@gmail.com>
2025-05-23 22:38:32 -0300

    updated logic

--- app/product/controller.py
+++ app/product/controller.py
@@ -1,21 +1,23 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, UploadFile
 from sqlmodel import Session
 from app.product.service import ProductService
 from app.utils.logger_class import LoggerClass
 from typing import List, Dict
+from sqlalchemy.exc import IntegrityError
 from app.schemas.response_model import DefaultResponse
+import json
 
 
 class ProductController:
     
     @staticmethod
-    async def get_not_on_sale_products(db: Session, page_number: int, page_size: int) -> List[Dict]:
+    def get_not_on_sale_products(db: Session, page_number: int, page_size: int) -> List[Dict]:
         """
-        Get all products.
+        Get not on sale products.
         """
         try:
             skip = (page_number - 1) * page_size
-            products_list = await ProductService.get_not_on_sale_products(db, skip, page_size)
+            products_list = ProductService.get_not_on_sale_products(db, skip, page_size)
             
             if not products_list:
                 raise HTTPException(
@@ -25,7 +27,7 @@ class ProductController:
             serialized_products = [product.model_dump() for product in products_list]
             return DefaultResponse(data=serialized_products, message="Products retrieved successfully")
 
-        except HTTPException as http_exc:
+        except HTTPException as http_exc:  
             # Re-raise HTTPExceptions (like the 404)
             raise http_exc
         except Exception as e:
@@ -37,13 +39,13 @@ class ProductController:
 
 
     @staticmethod
-    async def get_on_sale_products(db: Session, page_number: int, page_size: int) -> List[Dict]:
+    def get_on_sale_products(db: Session, page_number: int, page_size: int) -> List[Dict]:
         """
         Get on sale products.
         """
         try:
             skip = (page_number - 1) * page_size
-            products_list = await ProductService.get_on_sale_products(db, skip, page_size)
+            products_list = ProductService.get_on_sale_products(db, skip, page_size)  
 
             if not products_list:
                 raise HTTPException(
@@ -59,6 +61,60 @@ class ProductController:
         except Exception as e:  
             db.rollback()
             LoggerClass.error(f"Error getting on sale products: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal Server Error: {e}"
+            )
+
+    @staticmethod
+    async def import_products_fast(db: Session, file: UploadFile) -> DefaultResponse:
+        """
+        Bulk insert products into the database.
+        """
+        try:
+            content = await file.read()
+            products = json.loads(content)
+
+            if not isinstance(products, list):
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Invalid JSON format. Expected a list of products."
+                )
+
+            await ProductService.bulk_insert_products(db, products)
+            return DefaultResponse(data=None, message="Products imported successfully")
+
+        except IntegrityError as e:
+            db.roolback()
+            LoggerClass.error(f"Error importing products: {e}")
+            raise ValueError("One or more products violate unique constraints.")
+        except Exception as e:
+            db.rollback()
+            LoggerClass.error(f"Error importing products: {e}")
+            raise HTTPException(
+                status_code = 500,
+                detail=f"Failed to import products: {e}"
+            )
+
+    @staticmethod
+    def get_product_by_id(db: Session, id: int):
+        """
+        Get a product by its ID.
+        """
+        try:
+            product = ProductService.get_product_by_id(db, id)
+            if not product:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="Product not found"
+                )
+            serialized_product = product.model_dump()
+            return DefaultResponse(data=serialized_product, message="Product retrieved successfully")
+        
+        except HTTPException as http_exc:
+            # Re-raise HTTPExceptions (like the 404)
+            raise http_exc
+        except Exception as e:
+            db.rollback()
+            LoggerClass.error(f"Error getting product by id: {e}")
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal Server Error: {e}"
             )
--- app/product/model.py
+++ app/product/model.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, DateTime, LargeBinary
+from sqlalchemy import Column, JSON, LargeBinary
 from sqlmodel import Field
 from app.schemas.entity_model import Base
-from datetime import datetime, timezone
-from typing import Optional
+from typing import Optional, List
+
 
 class Product(Base, table=True):
     """
@@ -11,18 +11,26 @@ class Product(Base, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
 
-    name: str = Field(nullable=False, max_length=255)
+    filename: str = Field(nullable=False)
+    
+    barcode: str = Field(nullable=False, unique=True)
+
+    product_name: str = Field(nullable=False, max_length=255)
 
     description: Optional[str] = Field(default=None)
 
-    category: Optional[str] = Field(default=None, max_length=30)
+    category: str = Field(default=None, max_length=30)
 
-    subcategory: Optional[str] = Field(default=None, max_length=30)
+    subcategory: str = Field(default=None, max_length=30)
 
     price: float = Field(nullable=False)
 
-    stock: Optional[int] = Field(default=None)
+    sale_price: float = Field(default=None)
 
-    sale: Optional[bool] = Field(default=None)
+    sale_percentage: Optional[int] = Field(default=None)
 
-    byte_image: Optional[bytes] = Field(default=None, sa_column=Column(LargeBinary, nullable=True))
+    stock: int = Field(default=None)
+
+    on_sale: bool = Field(default=None)
+
+    embeddings: Optional[List[float]] = Field(default=None, sa_column=Column(JSON, nullable=True))
